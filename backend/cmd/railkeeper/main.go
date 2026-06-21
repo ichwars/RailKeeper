e29e3debde45304fa3de4086340f7fd4a38614b4
ichwars <drothe2012@gmail.com>
2026-06-21 22:09:40 +0200

    Set stable release version

--- backend/cmd/railkeeper/main.go
+++ backend/cmd/railkeeper/main.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	version               = "0.1.15-beta.1"
+	version               = "0.1.15"
 	defaultUpdateCheckURL = "https://api.github.com/repos/ichwars/RailKeeper/releases/latest"
 )
 
